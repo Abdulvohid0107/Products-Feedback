cc1ecbc88134563b1e2337c2843b8be573a5ee12
Abdulvohid0107 <sunnatillaabdulvohid@gmail.com>
2022-12-15 21:57:47 +0500

    problem solved

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -6,7 +6,7 @@ import "./login.scss";
 export const Login = () => {
   const { setLogin } = useContext(AuthContext);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const userNameRef = useRef();
   const passwordRef = useRef();
@@ -18,57 +18,30 @@ export const Login = () => {
       password: passwordRef.current.value,
     };
     setLogin(user);
-    navigate("/")
+    navigate("/");
   };
 
   return (
-    <div className="container">
-      <div className="screen">
-        <div className="screen__content">
-          <form autoComplete="off" className="login" onSubmit={handleLoginSubmit}>
-            <div className="login__field">
-              <i className="login__icon fas fa-user"></i>
-              <input
-                ref={userNameRef}
-                type="text"
-                className="login__input"
-                placeholder="User name / Email"
-                name="username"
-                required
-              />
-            </div>
-            <div className="login__field">
-              <i className="login__icon fas fa-lock"></i>
-              <input
-                ref={passwordRef}
-                type="password"
-                className="login__input"
-                placeholder="Password"
-                name="password"
-                required
-              />
-            </div>
-            <button className="button login__submit">
-              <span className="button__text">Log In Now</span>
-              <i className="button__icon fas fa-chevron-right"></i>
-            </button>
-          </form>
-          <div className="social-login">
-            <h3>log in via</h3>
-            {/* <div className="social-icons">
-              <a hrefLang="https://bfecglobal.uz" className="social-login__icon fab fa-instagram"></a>
-              <a hrefLang="https://bfecglobal.uz" className="social-login__icon fab fa-facebook"></a>
-              <a hrefLang="https://bfecglobal.uz" className="social-login__icon fab fa-twitter"></a>
-            </div> */}
-          </div>
+    <div className="login__body">
+      <h1 className="login__title">Login</h1>
+      <form className="login__form" onSubmit={handleLoginSubmit}>
+        <div className="row">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            autoComplete="off"
+            placeholder="email@example.com"
+            ref={userNameRef}
+            defaultValue="sunnatillaabdulvohid@gmail.com"
+          />
         </div>
-        <div className="screen__background">
-          <span className="screen__background__shape screen__background__shape4"></span>
-          <span className="screen__background__shape screen__background__shape3"></span>
-          <span className="screen__background__shape screen__background__shape2"></span>
-          <span className="screen__background__shape screen__background__shape1"></span>
+        <div className="row">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" ref={passwordRef} defaultValue="dontrepeatyourself" />
         </div>
-      </div>
+        <button className="login__button" type="submit">Login</button>
+      </form>
     </div>
   );
 };
